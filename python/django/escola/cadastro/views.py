4e31f1d1e6e3cee7b6c583cabbeb5f2079fd19bc
THIAGO ROBERTO SANTOS <thiago.roberto@aluno.senac.rio>
2025-02-05 19:34:41 -0300

    atualizando para entrega, estudos.

--- python/django/escola/cadastro/views.py
+++ python/django/escola/cadastro/views.py
@@ -1,7 +1,7 @@
 # Criar a view para listar alunos em cadastro/views.py
 
 from django.shortcuts import render
-from .models import Aluno,Curso, Turma
+from .models import Aluno, Curso, Turma
 from collections import Counter
 import json
 
@@ -32,16 +32,16 @@ def lista_alunos(request):
     turmas_values = list(turmas_count.values())
 
     # Criar estrutura de dados para os gráficos Sankey
-    sankey_curso_turma = []
-    for aluno in alunos:
-        for curso in aluno.cursos.all():
-            sankey_curso_turma.append([curso.nome, aluno.turma.nome, 1])
+    #sankey_curso_turma = []
+    #for aluno in alunos:
+    #    for curso in aluno.cursos.all():
+    #        sankey_curso_turma.append([curso.nome, aluno.turma.nome, 1])
 
-    sankey_aluno_curso_turma = []
-    for aluno in alunos:
-        for curso in aluno.cursos.all():
-            sankey_aluno_curso_turma.append([aluno.nome, curso.nome, 1])  # Aluno → Curso
-            sankey_aluno_curso_turma.append([curso.nome, aluno.turma.nome, 1])  # Curso → Turma
+    #sankey_aluno_curso_turma = []
+    #for aluno in alunos:
+    #    for curso in aluno.cursos.all():
+    #        sankey_aluno_curso_turma.append([aluno.nome, curso.nome, 1])  # Aluno → Curso
+    #        sankey_aluno_curso_turma.append([curso.nome, aluno.turma.nome, 1])  # Curso → Turma
 
     context = {
         'alunos': alunos,
@@ -49,8 +49,8 @@ def lista_alunos(request):
         'cursos_values': json.dumps(cursos_values),
         'turmas_labels': json.dumps(turmas_labels),
         'turmas_values': json.dumps(turmas_values),
-        'sankey_data_curso_turma': json.dumps(sankey_curso_turma),
-        'sankey_data_aluno_curso_turma': json.dumps(sankey_aluno_curso_turma),
+    #    'sankey_data_curso_turma': json.dumps(sankey_curso_turma),
+    #    'sankey_data_aluno_curso_turma': json.dumps(sankey_aluno_curso_turma),
     }
 
     return render(request, 'cadastro/lista_alunos.html', context)
